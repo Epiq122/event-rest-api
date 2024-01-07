fc9e36e2d43861b167cc76a7b9b66c1428ceb6d6
Robert Gleason <l2ob122@gmail.com>
2024-01-06 21:16:22 -0800

    Add getEvents and createEvent handlers to main.go and implement GetAllEvents function in models/event.go

--- main.go
+++ main.go
@@ -4,12 +4,25 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"robertgleason.ca/rest/models"
 )
 
 func getEvents(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Epiq Dev",
-	})
+	events := models.GetAllEvents()
+	c.JSON(http.StatusOK, events)
+}
+
+func createEvent(c *gin.Context) {
+	var event models.Event
+	err := c.ShouldBindJSON(&event)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	event.ID = 1
+	event.UserID = 1
+	c.JSON(http.StatusCreated, gin.H{"status": "success", "event": event})
 }
 
 func main() {
@@ -17,6 +30,7 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 
