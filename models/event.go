861af7efbea5d956cf6e81460cf97136d50d9300
Robert Gleason <l2ob122@gmail.com>
2024-01-07 17:58:55 -0800

    Refactor Event.Save() to save event data to the database

--- models/event.go
+++ models/event.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"robertgleason.ca/rest/db"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -13,9 +17,24 @@ type Event struct {
 
 var events = []Event{}
 
-func (e Event) Save() {
-	// later: save to db
-	events = append(events, e)
+func (e *Event) Save() error {
+	query := `INSERT INTO events (name, description, location, dateTime, user_id) VALUES (?, ?, ?, ?, ?)`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+	e.ID = id
+	return nil
 }
 
 func GetAllEvents() []Event {
